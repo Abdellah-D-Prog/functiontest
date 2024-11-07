35b4356aeb0378478f3de19094422c643b7ba76e
Abdellah-D-Prog <djezzar.abdellah@hotmail.com>
2024-11-07 11:52:57 +0100

    Exercice 1 et 2

---
+++ functions.js
@@ -0,0 +1,20 @@
+
+//-----------------Exercice 01------------------------------------------------------------------------------------//
+function hello()
+{
+    console.log("Bonjour"); //Affichage pour la fonctionnalité consolelog
+    console.log("Hello"); //Showcase with The consolelog option
+    return;
+}
+
+//j'appel ma fonction
+hello();
+//---------------Exercice 02--------------------------------------------------------------------------------------------------//
+
+function calcul(num1,num2)
+{
+    result=num1+num2;
+    console.log(result);
+    return result;
+}
+calcul(1,3);
